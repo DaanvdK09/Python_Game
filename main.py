65dd3e850bbef3da6c2bc2d1b0ab9f028bc3eb32
Bramborino <bram.blokker@outlook.com>
2025-10-06 14:19:37 +0200

    Changed Screen Height to test commit

--- main.py
+++ main.py
@@ -7,13 +7,16 @@ running = True
 
 #Screen
 Screen_Width=1285
-Screen_Height=700
+Screen_Height=800
 
 screen=pygame.display.set_mode((Screen_Width,Screen_Height))
 pygame.display.set_caption("Game")
 pygame.display.toggle_fullscreen()
 
 while running:
+
+
+
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
