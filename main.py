6616048e2a8bde2b2d9890e3f39003faa0ffab85
DaanvdK09 <van.der.kooij.daan@gmail.com>
2025-10-06 14:16:50 +0200

    Create window

---
+++ main.py
@@ -0,0 +1,21 @@
+import pygame
+
+pygame.init()
+
+#Int
+running = True
+
+#Screen
+Screen_Width=1285
+Screen_Height=700
+
+screen=pygame.display.set_mode((Screen_Width,Screen_Height))
+pygame.display.set_caption("Game")
+pygame.display.toggle_fullscreen()
+
+while running:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+
+pygame.quit()
