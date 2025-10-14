a5ce62ff579da9471541f29bbcc80764b4fe4c20
DaanvdK09 <van.der.kooij.daan@gmail.com>
2025-10-14 22:42:25 +0200

    Update Map

---
+++ Tilemap/Objects.tsx
@@ -0,0 +1,103 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.11.2" name="Objects" tilewidth="448" tileheight="448" tilecount="33" columns="0">
+ <grid orientation="orthogonal" width="1" height="1"/>
+ <tile id="33">
+  <image source="../graphics/objects/arean_fire.png" width="448" height="448"/>
+ </tile>
+ <tile id="34">
+  <image source="../graphics/objects/arena_plant.png" width="448" height="448"/>
+ </tile>
+ <tile id="35">
+  <image source="../graphics/objects/arena_water.png" width="448" height="448"/>
+ </tile>
+ <tile id="36">
+  <image source="../graphics/objects/gate_pillar.png" width="64" height="128"/>
+ </tile>
+ <tile id="37">
+  <image source="../graphics/objects/gate_top.png" width="232" height="80"/>
+ </tile>
+ <tile id="38">
+  <image source="../graphics/objects/grass.png" width="64" height="64"/>
+ </tile>
+ <tile id="39">
+  <image source="../graphics/objects/grass_ice.png" width="64" height="64"/>
+ </tile>
+ <tile id="40">
+  <image source="../graphics/objects/grassrock1.png" width="60" height="60"/>
+ </tile>
+ <tile id="41">
+  <image source="../graphics/objects/grassrock2.png" width="56" height="60"/>
+ </tile>
+ <tile id="42">
+  <image source="../graphics/objects/green_tree.png" width="128" height="192"/>
+ </tile>
+ <tile id="43">
+  <image source="../graphics/objects/green_tree_bushy.png" width="128" height="192"/>
+ </tile>
+ <tile id="44">
+  <image source="../graphics/objects/green_tree_small.png" width="64" height="128"/>
+ </tile>
+ <tile id="45">
+  <image source="../graphics/objects/hospital.png" width="384" height="384"/>
+ </tile>
+ <tile id="46">
+  <image source="../graphics/objects/house_large.png" width="448" height="448"/>
+ </tile>
+ <tile id="47">
+  <image source="../graphics/objects/house_large_alt.png" width="448" height="448"/>
+ </tile>
+ <tile id="48">
+  <image source="../graphics/objects/house_small.png" width="320" height="320"/>
+ </tile>
+ <tile id="49">
+  <image source="../graphics/objects/house_small_alt.png" width="320" height="320"/>
+ </tile>
+ <tile id="50">
+  <image source="../graphics/objects/ice_tree.png" width="128" height="192"/>
+ </tile>
+ <tile id="51">
+  <image source="../graphics/objects/icerock1.png" width="60" height="60"/>
+ </tile>
+ <tile id="52">
+  <image source="../graphics/objects/icerock2.png" width="56" height="60"/>
+ </tile>
+ <tile id="53">
+  <image source="../graphics/objects/palm.png" width="128" height="192"/>
+ </tile>
+ <tile id="54">
+  <image source="../graphics/objects/palm_alt.png" width="128" height="192"/>
+ </tile>
+ <tile id="55">
+  <image source="../graphics/objects/palm_small.png" width="64" height="128"/>
+ </tile>
+ <tile id="56">
+  <image source="../graphics/objects/ruin_gate.png" width="192" height="192"/>
+ </tile>
+ <tile id="57">
+  <image source="../graphics/objects/ruin_pillar.png" width="64" height="192"/>
+ </tile>
+ <tile id="58">
+  <image source="../graphics/objects/ruin_pillar_broke.png" width="64" height="104"/>
+ </tile>
+ <tile id="59">
+  <image source="../graphics/objects/ruin_pillar_broke_alt.png" width="64" height="128"/>
+ </tile>
+ <tile id="60">
+  <image source="../graphics/objects/sand.png" width="64" height="64"/>
+ </tile>
+ <tile id="61">
+  <image source="../graphics/objects/sandrock1.png" width="60" height="60"/>
+ </tile>
+ <tile id="62">
+  <image source="../graphics/objects/sandrock2.png" width="56" height="60"/>
+ </tile>
+ <tile id="63">
+  <image source="../graphics/objects/teal_tree.png" width="128" height="192"/>
+ </tile>
+ <tile id="64">
+  <image source="../graphics/objects/teal_tree_bushy.png" width="128" height="192"/>
+ </tile>
+ <tile id="65">
+  <image source="../graphics/objects/teal_tree_small.png" width="64" height="128"/>
+ </tile>
+</tileset>
