1e0ef679dd502a8a373f3799ea12e178fd51120e
Bramborino <bram.blokker@outlook.com>
2025-10-27 13:18:59 +0100

    Tried to add settings button

--- Script/UI/options.py
+++ Script/UI/options.py
@@ -21,27 +21,33 @@ def options_menu(screen, screen_width, screen_height, menu_font, colors, clock=N
 
         title_text = title_font.render("Options", True, GOLD)
         fullscreen_text = menu_font.render("Toggle Fullscreen", True, BLACK)
+        settings_text = menu_font.render("Settings", True, BLACK)
         back_text = menu_font.render("Back", True, BLACK)
         quit_text = menu_font.render("Quit", True, BLACK)
 
         title_y = int(sh * 0.12)
         fullscreen_y = sh // 2 - 60
+        settings_y = fullscreen_y + 60
         back_y = fullscreen_y + 120
         quit_y = back_y + 120
 
         fullscreen_rect = pygame.Rect(cx - fullscreen_text.get_width()//2 - 10, fullscreen_y - 10,
                                       fullscreen_text.get_width() + 20, fullscreen_text.get_height() + 20)
+        settings_rect = pygame.Rect(cx - back_text.get_width()//2 - 10, back_y - 10,
+                                back_text.get_width() + 20, back_text.get_height() + 20)
         back_rect = pygame.Rect(cx - back_text.get_width()//2 - 10, back_y - 10,
                                 back_text.get_width() + 20, back_text.get_height() + 20)
         quit_rect = pygame.Rect(cx - quit_text.get_width()//2 - 10, quit_y - 10,
                                 quit_text.get_width() + 20, quit_text.get_height() + 20)
 
         pygame.draw.rect(screen, GOLD, fullscreen_rect, border_radius=12)
+        pygame.draw.rect(screen, GOLD, settings_rect, border_radius=12)
         pygame.draw.rect(screen, GOLD, back_rect, border_radius=12)
         pygame.draw.rect(screen, GOLD, quit_rect, border_radius=12)
 
         screen.blit(title_text, (cx - title_text.get_width()//2, title_y))
         screen.blit(fullscreen_text, (cx - fullscreen_text.get_width()//2, fullscreen_y))
+        screen.blit(settings_text, (cx - settings_text.get_width()//2, fullscreen_y + 60))
         screen.blit(back_text, (cx - back_text.get_width()//2, back_y))
         screen.blit(quit_text, (cx - quit_text.get_width()//2, quit_y))
 
